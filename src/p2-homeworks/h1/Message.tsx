899cd13d58a5392d284654ec68d64d1d5f1a44da
Alexander Stepanov <asrebel777@gmail.com>
2022-03-04 21:15:25 +0500

    Must to do CSS

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import s from './Message.module.css'
 
-function Message() {
+export type MessagePropsType = {
+    avatar: string;
+    name: string;
+    message: string;
+    time: string;
+}
+
+
+const Message = (props: MessagePropsType) => {
     return (
-        <div>
-
+        <div className = {s.message}>
+            <img src={props.avatar} alt='#'/>
+            <div>
+                <h5>{props.name}</h5>
+                <p>{props.message}</p>
+                <p>{props.time}</p>
+            </div>
         </div>
     )
 }
