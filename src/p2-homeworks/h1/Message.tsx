e92ad982e9eec4513aa862798f9e9122621117ad
Alexander Stepanov <asrebel777@gmail.com>
2022-03-04 22:55:40 +0500

    HW1 done

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -13,10 +13,10 @@ const Message = (props: MessagePropsType) => {
     return (
         <div className = {s.message}>
             <img src={props.avatar} alt='#'/>
-            <div>
-                <h5>{props.name}</h5>
-                <p>{props.message}</p>
-                <p>{props.time}</p>
+            <div className = {s.text}>
+                <h5 className={s.name}>{props.name}</h5>
+                <p className={s.sms}>{props.message}</p>
+                <p className={s.time}>{props.time}</p>
             </div>
         </div>
     )
